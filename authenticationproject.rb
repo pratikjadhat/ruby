46b7e4876ac7486b3ddc93e9f4a6e1387e959069
Pratik Jadhar <pratikjadhar2326@gmail.com>
2025-02-22 20:08:45 +0530

    Added myfolder to the repository

--- authenticationproject.rb
+++ authenticationproject.rb
@@ -1,37 +1,42 @@
 users =  [
-        {username:"Pratik" , password:"p123"},
-        {username:"Sumit" , password:"s123"},
-        {username:"Rahul" , password:"r123"},
-        {username:"Megha" , password:"m123"}
-        ]
+    {username: "Pratik", password: "p123"},
+    {username: "Sumit", password: "s123"},
+    {username: "Rahul", password: "r123"},
+    {username: "Megha", password: "m123"}
+]
 
-        puts "WELCOME TO AN AUTHENTICATION"
-        50.times {print"_"}
-        puts
-        puts "This Program will take input from user and compare password"
-        puts "If password is correct you will et back to user object"
+puts "WELCOME TO AN AUTHENTICATION"
+50.times { print "_" }
+puts
+puts "This Program will take input from user and compare password"
+puts "If password is correct you will get back to user object"
 
-        while true
-          print "username: " 
-          username = gets.chomp
-          print "Password: "
-          password = gets.chomp
+while true
+    print "username: " 
+    username = gets.chomp
+    print "Password: "
+    password = gets.chomp
 
-          users.each do |user|
-          
-            if user[:username] == username && user[:password] == password
+    # Flag to check if credentials are correct
+    found = false
 
-               puts "Correct credential #{user}"
-               break
-            else
-                puts "Plz enter correct PAssword and USer"  
-                break
-            end
-            
-          end
+    # Loop through all users
+    users.each do |user|
+        if user[:username] == username && user[:password] == password
+            puts "Correct credentials! #{user}"
+            found = true
+            break # If match is found, exit the loop early
+        end
+    end
 
-            puts "Press n or N to end or any other key to continue"
-            con =gets.chomp
-            break if con=="n" || con=="N"
-        
-      end
+    # If no user match is found, show an error message
+    if !found
+        puts "Please enter correct username and password."
+    end
+
+    puts "Press n or N to end or any other key to continue"
+    con = gets.chomp
+    50.times { print "_" }
+    puts
+    break if con == "n" || con == "N"
+end
